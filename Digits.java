c87a61a03dcb8f6786de2f5435c162496b037960
IYUN71639-max <iyun@stu.socsd.org>
2025-02-27 09:49:45 -0500

    Update Digits.java

--- Digits.java
+++ Digits.java
@@ -1,5 +1,6 @@
 // Isaac Yun, February 25th, Digits.java, 
 // take a 3 digit number and seperate each of the digits
+import java.util.*;
 public class Digits {
 
 
@@ -10,13 +11,16 @@ public class Digits {
         int tens;
         int hundreds;
         int thousands;
-        number = 2580;
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a 4-digit number!!!!!!!: ");
+        number = input.nextInt();
+        //number = 2568;
         ones = number % 10;
         tens = (number / 10) % 10;
         hundreds = ((number / 10)/10)%10;
         thousands = (((number / 10)/10)/10)%10;
-        System.out.println("The ones digit is " + ones + ", the tens digit is " + tens + ", the hundreds digit is " + hundreds + ", the thousands digit is " + thousands + ".");
+        System.out.println("The ones digit is " + ones + ", the tens digit is " + tens + ", the hundreds digit is " + hundreds + ", the thousands digit is " + thousands + "!!!!!!!!!!!!!!!");
     }
 }
 
